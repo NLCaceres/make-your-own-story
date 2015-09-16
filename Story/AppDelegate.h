be5d83227ed42f731058f039c3197a97b875b2a9
Lionel Caceres <lkcaceres@guest-wireless-207-151-035-079.usc.edu>
2015-09-16 12:56:02 -0700

    Initial Commit

---
+++ Story/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Story
+//
+//  Created by Lionel Caceres on 9/16/15.
+//  Copyright (c) 2015 Nicholas Caceres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
